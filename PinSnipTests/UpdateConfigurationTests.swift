61268ea2e32c301de094d6479a4f9d4cb8be5276
longhai <longhaiqwe@gmail.com>
2026-08-01 09:36:08 +0800

    chore: publish v0.7.1 appcast

--- PinSnipTests/UpdateConfigurationTests.swift
+++ PinSnipTests/UpdateConfigurationTests.swift
@@ -57,15 +57,15 @@ final class UpdateConfigurationTests: XCTestCase {
         )
 
         XCTAssertTrue(appcast.contains("<item>"))
-        XCTAssertTrue(appcast.contains("<sparkle:version>8</sparkle:version>"))
+        XCTAssertTrue(appcast.contains("<sparkle:version>9</sparkle:version>"))
         XCTAssertTrue(
             appcast.contains(
-                "<sparkle:shortVersionString>0.7.0</sparkle:shortVersionString>"
+                "<sparkle:shortVersionString>0.7.1</sparkle:shortVersionString>"
             )
         )
         XCTAssertTrue(
             appcast.contains(
-                "releases/download/v0.7.0/PinSnip-v0.7.0-macOS-universal.zip"
+                "releases/download/v0.7.1/PinSnip-v0.7.1-macOS-universal.zip"
             )
         )
         XCTAssertTrue(appcast.contains("sparkle:edSignature="))
@@ -80,14 +80,14 @@ final class UpdateConfigurationTests: XCTestCase {
         XCTAssertTrue(
             appcast.contains(
                 "<sparkle:fullReleaseNotesLink>"
-                    + "https://github.com/longhaiqwe/PinSnip/releases/tag/v0.7.0"
+                    + "https://github.com/longhaiqwe/PinSnip/releases/tag/v0.7.1"
                     + "</sparkle:fullReleaseNotesLink>"
             )
         )
         XCTAssertFalse(
             appcast.contains(
                 "raw.githubusercontent.com/longhaiqwe/PinSnip/main/"
-                    + "PinSnip-v0.7.0-macOS-universal.md"
+                    + "PinSnip-v0.7.1-macOS-universal.md"
             )
         )
     }
